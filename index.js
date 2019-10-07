97fdfe41ad5c5e16ddb1c2b02921f8b61204455d
onepixel <onepixel@126.com>
2019-10-07 16:22:15 +0800

    feat: add rules

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 module.exports = {
   root: true,
+
   env: {
     browser: true,
     es6: true,
   },
+
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
   ],
+
   parser: 'vue-eslint-parser',
+
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+
   plugins: [
     'vue',
   ],
+
   rules: {
     'no-console': 0,
+    'no-debugger': 0,
     'linebreak-style': 0,
+    'class-methods-use-this': 0,
+    'no-param-reassign': 0,
+    'global-require': 0,
+    'no-new': 0,
     'import/no-extraneous-dependencies': 0,
     'import/order': 0,
   },
+
   settings: {
     'import/ignore': ['node_modules'],
-    'import/extensions': ['.js', '.vue']
+    'import/extensions': ['.js', '.vue'],
   },
 };
